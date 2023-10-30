73e1a35187d5e45d82525faf9a5b5e1ac3e520c7
dcoder <dcoder1745@gmail.com>
2023-10-31 02:42:40 +0600

    02:42 31.10.2023 правки

--- src/components/pages/states/States.jsx
+++ src/components/pages/states/States.jsx
@@ -11,7 +11,7 @@ export default () => {
             <a href="#">
               View jobs
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                <path d="M10 17.5L15 12L10 6.5" stroke="#606C77" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M10 17.5L15 12L10 6.5" stroke="currentColor" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
               </svg>
             </a>
           </li>
