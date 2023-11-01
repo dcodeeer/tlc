31cb1c490f61ddbfe1073dfa60cc0e840676fc32
dcoder <dcoder1745@gmail.com>
2023-11-02 00:21:56 +0600

    00:21 02.11.2023 add animation + links

--- public/scripts/animation.js
+++ public/scripts/animation.js
@@ -130,3 +130,171 @@ gsap.fromTo(
   { y: 200, opacity: 0 },
   { y: 0, opacity: 1, delay: indexSixth.delay, scrollTrigger: indexSixth.trigger },
 );
+
+// index seventh
+
+const indexSeventh = {};
+indexSeventh.trigger = {
+  trigger: '.index-page .seventh',
+  start: '0px bottom',
+  end: 'bottom top',
+  toggleActions: "play pause resume reset"
+};
+indexSeventh.delay = 0.6;
+
+gsap.fromTo(
+  '.index-page .seventh .title',
+  { y: 200, opacity: 0 },
+  { y: 0, opacity: 1, delay: indexSeventh.delay, scrollTrigger: indexSeventh.trigger },
+);
+
+gsap.fromTo(
+  '.index-page .seventh .list',
+  { y: 200, opacity: 0 },
+  { y: 0, opacity: 1, delay: indexSeventh.delay + 0.1, scrollTrigger: indexSeventh.trigger },
+);
+
+gsap.fromTo(
+  '.index-page .seventh .more',
+  { y: 200, opacity: 0 },
+  { y: 0, opacity: 1, delay: indexSeventh.delay + 0.2, scrollTrigger: indexSeventh.trigger },
+);
+
+// index eighth
+
+const indexEighth = {};
+indexEighth.trigger = {
+  trigger: '.index-page .eighth',
+  start: '0px bottom',
+  end: 'bottom top',
+  toggleActions: "play pause resume reset"
+};
+indexEighth.delay = 0.6;
+
+gsap.fromTo(
+  '.index-page .eighth .title',
+  { y: 100, opacity: 0 },
+  { y: 0, opacity: 1, delay: indexEighth.delay, scrollTrigger: indexEighth.trigger },
+);
+
+gsap.fromTo(
+  '.index-page .eighth img',
+  { x: -100, opacity: 0 },
+  { x: 0, opacity: 1, delay: indexEighth.delay, scrollTrigger: indexEighth.trigger },
+);
+
+gsap.fromTo(
+  '.index-page .eighth .box',
+  { y: 200, opacity: 0 },
+  { y: 0, opacity: 1, delay: indexEighth.delay, scrollTrigger: indexEighth.trigger },
+);
+
+
+// index ninth
+
+const indexNinth = {};
+indexNinth.trigger = {
+  trigger: '.index-page .ninth',
+  start: '0px bottom',
+  end: 'bottom top',
+  toggleActions: "play pause resume reset"
+};
+indexNinth.delay = 0.6;
+
+gsap.fromTo(
+  '.index-page .ninth .title',
+  { y: 200, opacity: 0 },
+  { y: 0, opacity: 1, delay: indexNinth.delay, scrollTrigger: indexNinth.trigger },
+);
+
+gsap.fromTo(
+  '.index-page .ninth .slider-wrapper',
+  { y: 200, opacity: 0 },
+  { y: 0, opacity: 1, delay: indexNinth.delay + 0.1, scrollTrigger: indexNinth.trigger },
+);
+
+gsap.fromTo(
+  '.index-page .ninth .more',
+  { y: 100, opacity: 0 },
+  { y: 0, opacity: 1, delay: indexNinth.delay + 0.2, scrollTrigger: indexNinth.trigger },
+);
+
+// index tenth
+
+const indexTenth = {};
+indexTenth.trigger = {
+  trigger: '.index-page .tenth',
+  start: '0px bottom',
+  end: 'bottom top',
+  toggleActions: "play pause resume reset"
+};
+indexTenth.delay = 0.6;
+
+gsap.fromTo(
+  '.index-page .tenth .header',
+  { y: 200, opacity: 0 },
+  { y: 0, opacity: 1, delay: indexTenth.delay, scrollTrigger: indexTenth.trigger },
+);
+
+gsap.fromTo(
+  '.index-page .tenth .slider',
+  { y: 200, opacity: 0 },
+  { y: 0, opacity: 1, delay: indexTenth.delay + 0.1, scrollTrigger: indexTenth.trigger },
+);
+
+// index eleventh
+
+const indexEleventh = {};
+indexEleventh.trigger = {
+  trigger: '.index-page .eleventh',
+  start: '0px bottom',
+  end: 'bottom top',
+  toggleActions: 'play pause resume reset'
+};
+indexEleventh.delay = 0.6;
+
+gsap.fromTo(
+  '.index-page .eleventh .title',
+  { y: 200, opacity: 0 },
+  { y: 0, opacity: 1, delay: indexEleventh.delay, scrollTrigger: indexEleventh.trigger },
+);
+
+gsap.fromTo(
+  '.index-page .eleventh .form',
+  { y: 200, opacity: 0 },
+  { y: 0, opacity: 1, delay: indexEleventh.delay + 0.1, scrollTrigger: indexEleventh.trigger },
+);
+
+gsap.fromTo(
+  '.index-page .eleventh .slider-wrapper',
+  { y: 200, opacity: 0 },
+  { y: 0, opacity: 1, delay: indexEleventh.delay + 0.2, scrollTrigger: indexEleventh.trigger },
+);
+
+gsap.fromTo(
+  '.index-page .eleventh .more',
+  { y: 100, opacity: 0 },
+  { y: 0, opacity: 1, delay: indexEleventh.delay + 0.3, scrollTrigger: indexEleventh.trigger },
+);
+
+// index twelfth 
+const indexTwelfth = {};
+indexTwelfth.trigger = {
+  trigger: '.index-page .twelfth',
+  start: '0px bottom',
+  end: 'bottom top',
+  toggleActions: 'play pause resume reset'
+};
+indexTwelfth.delay = 0.6;
+
+gsap.fromTo(
+  '.index-page .twelfth .title',
+  { y: 200, opacity: 0 },
+  { y: 0, opacity: 1, delay: indexTwelfth.delay, scrollTrigger: indexTwelfth.trigger },
+);
+
+gsap.fromTo(
+  '.index-page .twelfth .list',
+  { y: 200, opacity: 0 },
+  { y: 0, opacity: 1, delay: indexTwelfth.delay + 0.1, scrollTrigger: indexTwelfth.trigger },
+);
